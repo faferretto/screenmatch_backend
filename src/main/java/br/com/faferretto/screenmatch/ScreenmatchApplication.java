bd3229215170c191b7b8e4dd5188dc3d30b59a74
Fabio Ferretto <fabioandradeferretto@gmail.com>
2024-02-26 18:01:20 -0300

    Versão 2 -> aula 4

--- src/main/java/br/com/faferretto/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/faferretto/screenmatch/ScreenmatchApplication.java
@@ -1,8 +1,6 @@
 package br.com.faferretto.screenmatch;
 
-import br.com.faferretto.screenmatch.model.DadosSerie;
-import br.com.faferretto.screenmatch.service.ConsumoAPI;
-import br.com.faferretto.screenmatch.service.ConverteDados;
+import br.com.faferretto.screenmatch.principal.Principal;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -16,11 +14,7 @@ public class ScreenmatchApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		var consumoAPI = new ConsumoAPI();
-		var json = consumoAPI.obterDados("http://www.omdbapi.com/?t=gilmore+girls&apikey=dcde6eb8");
-		System.out.println(json);
-		ConverteDados conversor = new ConverteDados();
-		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
-		System.out.println(dados);
+		Principal principal = new Principal();
+		principal.exibeMenu();
 	}
 }
