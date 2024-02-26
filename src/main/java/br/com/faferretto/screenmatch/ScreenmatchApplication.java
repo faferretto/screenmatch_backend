c5ac13e0ea26cb96f089674c5a066cee4d167946
Fabio Ferretto <fabioandradeferretto@gmail.com>
2024-02-26 14:45:15 -0300

    Inicio do projeto spring

---
+++ src/main/java/br/com/faferretto/screenmatch/ScreenmatchApplication.java
@@ -0,0 +1,26 @@
+package br.com.faferretto.screenmatch;
+
+import br.com.faferretto.screenmatch.model.DadosSerie;
+import br.com.faferretto.screenmatch.service.ConsumoAPI;
+import br.com.faferretto.screenmatch.service.ConverteDados;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenmatchApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenmatchApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		var consumoAPI = new ConsumoAPI();
+		var json = consumoAPI.obterDados("http://www.omdbapi.com/?t=gilmore+girls&apikey=dcde6eb8");
+		System.out.println(json);
+		ConverteDados conversor = new ConverteDados();
+		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+		System.out.println(dados);
+	}
+}
